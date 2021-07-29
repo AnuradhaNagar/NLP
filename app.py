15049615c9f2b5ad3ae31e41effc3c712339485c
AnuradhaNagar <74099638+AnuradhaNagar@users.noreply.github.com>
2021-07-29 23:48:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    df = pd.read_csv('F:/Project/NLP-Emails-abusive-nonabusive--master/P38-NLP-Emails/emails.csv')
+    df = pd.read_csv('D:\Emails.csv.csv')
     df = df.drop(['Unnamed: 0', 'filename', 'Message-ID'], axis = 1)
     df['label'] = df['Class'].map({'Non Abusive': 0, 'Abusive': 1})
     
